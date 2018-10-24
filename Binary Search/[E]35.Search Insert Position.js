b4a9afdafb86b690ffe5276373f5128bd17234ce
unsad <wzxaylryw@hotmail.com>
2018-10-25 00:05:02 +0800

    35题解

---
+++ Binary Search/[E]35.Search Insert Position.js
@@ -0,0 +1,22 @@
+ /**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left <= right) {
+    let mid = left + Math.floor((right - left) / 2);
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return left;
+};
+
+/**
+ * 找出合适的插入位置，基础二分
+ */

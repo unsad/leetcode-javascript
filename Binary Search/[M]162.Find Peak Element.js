41e201c48a4b1b86e364cfd105650d8d02e1b89a
unsad <wzxaylryw@hotmail.com>
2018-10-26 00:26:53 +0800

    162题解

---
+++ Binary Search/[M]162.Find Peak Element.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left <= right) {
+    let mid = left + Math.floor((right - left) / 2);
+    if (nums[mid] < nums[mid + 1]) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return left;
+};
+
+/**
+ * 二分搜索分界条件和前一个值比大小即可
+ */
